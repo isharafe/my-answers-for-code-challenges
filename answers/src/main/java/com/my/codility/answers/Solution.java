86adf48f34c59f2116e545a32e42e9e80d1fc630
Ruchira <ruchira@coredevone.com>
2022-08-22 15:20:29 +0530

    my solution

--- answers/src/main/java/com/my/codility/answers/Solution.java
+++ answers/src/main/java/com/my/codility/answers/Solution.java
@@ -1,12 +1,103 @@
 package com.my.codility.answers;
 
+import java.util.Arrays;
+
 /**
  * Hello world!
  *
  */
 public class Solution {
 
-	public void solution(int[] A) {
+	public int solution(int[] X, int[] Y, String colors) {
 
+		// easy colors are in an array
+		char[] cArr = colors.toCharArray();
+
+		/*
+		 * now data are in different places, X,Y,colors ! It easy to work when all
+		 * realated data are in one place. Using DataPoint class for that
+		 */
+		DataPoint[] data = new DataPoint[X.length];
+		for (int i = 0; i < X.length; i++) {
+			data[i] = new DataPoint(X[i], Y[i], cArr[i]);
+		}
+
+		/*
+		 * we want the maximum number of points in the circle.
+		 * sort by radius helps to find this
+		 */
+		Arrays.sort(data, (a, b) -> {
+			return Integer.compare(a.r2, b.r2);
+		});
+
+		// initial answer
+		int ans = 0;
+
+		/*
+		 * count red and green point count at each data point
+		 */
+		int[] rCount = new int[data.length];
+		int[] gCount = new int[data.length];
+		int r = 0;
+		int g = 0;
+		for (int i = 0; i < data.length; i++) {
+			DataPoint cur = data[i];
+
+			if (cur.c == 'R') {
+				r++;
+			}
+			if (cur.c == 'G') {
+				g++;
+			}
+
+			rCount[i] = r;
+			gCount[i] = g;
+		}
+
+		/*
+		 * data array is sorted by radius.
+		 * now check the R & G point from max radius to min radius
+		 * if R & G counts match, "i" at that point (actually i+1 since i is 0 based)
+		 * is the answer
+		 */
+		for (int i = data.length - 1; i >= 0; i--) {
+
+			// skip first iteration, no i+1 for first iteration
+			/* there can be several points with same radius.
+			 * the correct R & G count is in the latest element of the
+			 * array. so after checking the latest element in array,
+			 *  skipping other points with same radius
+			 */
+			if (i != (data.length - 1) && data[i].r2 == data[i + 1].r2) {
+				continue;
+			}
+
+			if (rCount[i] == gCount[i]) {
+				ans = i + 1;
+				break;
+			}
+		}
+
+		return ans;
+	}
+
+	static class DataPoint {
+
+		public int x;
+		public int y;
+		public char c;
+		public int r2;
+
+		public DataPoint(int x, int y, char c) {
+			this.x = x;
+			this.y = y;
+			this.c = c;
+			this.r2 = (x * x) + (y * y);
+		}
+
+		@Override
+		public String toString() {
+			return String.format("x=%s y=%s c=%s r2=%s", x, y, c, r2);
+		}
 	}
 }
--- answers/src/test/java/com/my/codility/answers/SolutionTest.java
+++ answers/src/test/java/com/my/codility/answers/SolutionTest.java
@@ -1,5 +1,7 @@
 package com.my.codility.answers;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 /**
@@ -11,5 +13,34 @@ public class SolutionTest {
 	public void testSolution() {
 		Solution s = new Solution();
 
+		int[] X1 = new int[] {4, 0, 2, -2};
+		int[] Y1 = new int[] {4, 1, 2, -3};
+		String colors1 = "RGRR";
+		int solution1 = s.solution(X1, Y1, colors1);
+		assertEquals(solution1, 2);
+
+		int[] X2 = new int[] {1, 1, -1, -1};
+		int[] Y2 = new int[] {1, -1, 1, -1};
+		String colors2 = "RGRG";
+		int solution2 = s.solution(X2, Y2, colors2);
+		assertEquals(solution2, 4);
+
+		int[] X3 = new int[] {1, 0, 0};
+		int[] Y3 = new int[] {0, 1, -1};
+		String colors3 = "GGR";
+		int solution3 = s.solution(X3, Y3, colors3);
+		assertEquals(solution3, 0);
+
+		int[] X4 = new int[] {5, -5, 5};
+		int[] Y4 = new int[] {1, -1, -3};
+		String colors4 = "GRG";
+		int solution4 = s.solution(X4, Y4, colors4);
+		assertEquals(solution4, 2);
+
+		int[] X5 = new int[] {3000, -3000, 4100, -4100, -3000};
+		int[] Y5 = new int[] {5000, -5000, 4100, -4100, 5000};
+		String colors5 = "RRGRG";
+		int solution5 = s.solution(X5, Y5, colors5);
+		assertEquals(solution5, 2);
 	}
 }
