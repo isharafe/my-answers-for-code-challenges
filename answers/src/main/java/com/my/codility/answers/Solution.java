c6935dedbb4d022d6d6f3503e7bcd70690e27b3e
Ruchira <ruchira@coredevone.com>
2022-08-22 15:32:26 +0530

    my solution

--- answers/src/main/java/com/my/codility/answers/Solution.java
+++ answers/src/main/java/com/my/codility/answers/Solution.java
@@ -1,12 +1,48 @@
 package com.my.codility.answers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Hello world!
- *
+ * Not sure this solution is correct. I failed at the initial test and later I
+ * finish this solution. Couldn't test this in codility
  */
 public class Solution {
 
-	public void solution(int[] A) {
+	public int[] solution(int[] A) {
+		int base = -2;
+		int val = binaryToNumber(A, base);
+		int ceil = (int) Math.ceil((val) / 2.0);
+		return numberToBinary(ceil, base);
+	}
 
+	private int binaryToNumber(int[] arr, int base) {
+		int sum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			sum += arr[i] * Math.pow(base, i);
+		}
+		return sum;
+	}
+
+	private int[] numberToBinary(int num, int base) {
+		List<Integer> res = new ArrayList<>();
+		int aBase = Math.abs(base);
+		int aNum = Math.abs(num);
+
+		while (aNum >= aBase || num < 0) {
+			int mod = num % base;
+			int aMod = Math.abs(mod);
+			num = (num - aMod) / base;
+			aNum = Math.abs(num);
+			res.add(aMod);
+		}
+		res.add(aNum);
+
+		int[] arr = new int[res.size()];
+		for (int i = 0; i < res.size(); i++) {
+			arr[i] = res.get(i);
+		}
+
+		return arr;
 	}
 }
--- answers/src/test/java/com/my/codility/answers/SolutionTest.java
+++ answers/src/test/java/com/my/codility/answers/SolutionTest.java
@@ -1,5 +1,7 @@
 package com.my.codility.answers;
 
+import static org.junit.Assert.assertArrayEquals;
+
 import org.junit.Test;
 
 /**
@@ -8,8 +10,24 @@ import org.junit.Test;
 public class SolutionTest {
 
 	@Test
+	/*
+	 * assert answers here are "my answers",
+	 * don't remember the correct answers from the test :P
+	 */
 	public void testSolution() {
 		Solution s = new Solution();
 
+		int[] A1 = new int[] {1,0,0,1,1,1}; // = -23
+		int[] solution1 = s.solution(A1);
+		assertArrayEquals(solution1, new int[] {1,0,1,0,1,1});
+
+		int[] A2 = new int[] {0,0,1}; // = 4
+		int[] solution2 = s.solution(A2);
+		assertArrayEquals(solution2, new int[] {0,1,1});
+
+		int[] A3 = new int[] {1,0,0,1,1}; // = 9
+		int[] solution3 = s.solution(A3);
+		assertArrayEquals(solution3, new int[] {1,0,1});
 	}
+
 }
