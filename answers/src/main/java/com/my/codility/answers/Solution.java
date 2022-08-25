357f30234235e0b75514de1797350682f3f76acc
Ruchira <ruchira@coredevone.com>
2022-08-25 17:12:22 +0530

    solution v3 - got the idea from an internet artical. link included in the class comment

--- answers/src/main/java/com/my/codility/answers/Solution.java
+++ answers/src/main/java/com/my/codility/answers/Solution.java
@@ -1,37 +1,76 @@
 package com.my.codility.answers;
 
+import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.Map;
 
 /**
  * This is a HackerRank test
  *
  * my (late) solution for the Mercari test.
  *
+ * got this solution from an online article
+ * https://www.geeksforgeeks.org/smallest-subarray-k-distinct-numbers/
+ *
+ * think of a window on top of the array. initially window width is 0 window
+ * expand until it has "K" distinct numbers inside it. At this point we have our
+ * first sub-array with K elements. enter each number to a map. map's key size
+ * is distinct numbers and put the occurrence of each number in value shift
+ * window 1 point to the right the number at the left margin disappear from
+ * window and a new number enters from right side reduce left number from the
+ * map add right number to the map
  */
 public class Solution {
 
 	public int solution(List<Integer> arr, int k) {
-		int minimumLength = -1;
-		for(int i=0; i<arr.size(); i++) {
-			int subArrayMinEnd = i + k;
-			for(int j=subArrayMinEnd; j<=arr.size(); j++) {
-				Set<Integer> uniques = findUniques(arr, i, j);
-				if(uniques.size()==k) {
-					// there won't be a minimum than k
-					return k;
+		Map<Integer, Integer> numCount = new HashMap<>();
+
+		int minWindowWidth = arr.size();
+//
+//		int _start=0, _end=0;
+//		while(true) {
+//			if(_end < arr.size()) {
+//				_end++;
+//			} else {
+//				_start++;
+//			}
+//
+//			break;
+//		}
+
+		for (int start = 0, end = start; end < arr.size(); end++) {
+			numCount.put(arr.get(end), numCount.getOrDefault(arr.get(end), 0) + 1);
+
+			// if we have k distinct numbers
+			while (numCount.size() == k) {
+				// if current window length is less than previous minimum window length
+				int currentWindowLength = end - start + 1; // +1 since end is 0 based
+				if (currentWindowLength < minWindowWidth && currentWindowLength >= k) {
+					minWindowWidth = currentWindowLength;
 				}
-				if(uniques.size() > k && uniques.size() < minimumLength) {
-					minimumLength = uniques.size();
+
+				if(minWindowWidth == k) {
+					// there won't be a minimum window less than k
+					return minWindowWidth;
 				}
+
+				/*
+				 * shift window to right by one.
+				 * remove the left most number from the map.
+				 */
+
+				int leftMostNumberInWindow = arr.get(start);
+				int leftMostNumberCount = numCount.get(leftMostNumberInWindow);
+				if (leftMostNumberCount == 1) {
+					numCount.remove(leftMostNumberInWindow);
+				} else {
+					numCount.put(leftMostNumberInWindow, leftMostNumberCount - 1);
+				}
+
+				start++;
 			}
 		}
 
-		return minimumLength;
-	}
-
-	public static Set<Integer> findUniques(List<Integer> arr, int from, int to) {
-		return arr.subList(from, to).stream().collect(Collectors.toSet());
+		return (minWindowWidth == arr.size() && numCount.size() < k) ? -1 : minWindowWidth;
 	}
 }
