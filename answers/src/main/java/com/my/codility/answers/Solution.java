053408113cc5871afb4be7330e5d67e72ba50249
Ruchira <ruchira@coredevone.com>
2022-09-06 02:05:50 +0530

    use a binary tree to keep track of numbers. after max numbers filled, remove the lease number and take the next number in

--- answers/src/main/java/com/my/codility/answers/Solution.java
+++ answers/src/main/java/com/my/codility/answers/Solution.java
@@ -1,12 +1,125 @@
 package com.my.codility.answers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
  *
  */
 public class Solution {
 
-	public void solution(int[] A) {
+	static class BTree {
+		public static int MAX_COUNT = 1000;
 
+		int count;
+		BNode root;
+		BNode minValue;
+
+		public boolean add(int value) {
+			return add(new BNode(value));
+		}
+
+		private boolean add(BNode node) {
+
+			if (count == MAX_COUNT) {
+				if(node.value <= minValue.value) {
+					return false;
+				}
+
+				// we have max count of numbers
+				// if this value is larger than current min value,
+				// remove the minNode and add this
+				if(minValue == root) {
+					root = minValue.right;
+					root.parent = null;
+				} else {
+					minValue.parent.left = null;
+					if(minValue.right != null) {
+						minValue.parent.add(minValue.right);
+					}
+				}
+
+				// go to the new min value
+				minValue = minValue.parent;
+				while (minValue.left != null) {
+					minValue = minValue.left;
+				}
+
+				count--;
+			}
+
+			count++;
+			if (root == null) {
+				root = node;
+				minValue = root;
+			} else {
+				root.add(node);
+			}
+
+			if (minValue.value > node.value) {
+				minValue = node;
+			}
+
+			return true;
+		}
+
+		public List<Integer> toList() {
+			List<Integer> ret = new ArrayList<Integer>(MAX_COUNT);
+			BNode node = root;
+			fillList(ret, node);
+			return ret;
+		}
+
+		private void fillList(List<Integer> list, BNode node) {
+			if (node.right != null) {
+				fillList(list, node.right);
+			}
+			list.add(node.value);
+			if (node.left != null) {
+				fillList(list, node.left);
+			}
+		}
 	}
+
+	static class BNode {
+		int value;
+
+		private BNode parent;
+		private BNode left;
+		private BNode right;
+
+		public BNode(int value) {
+			this.value = value;
+		}
+
+		public void add(BNode node) {
+			if (node.value > this.value) {
+				if (this.right == null) {
+					this.right = node;
+					node.parent = this;
+				} else {
+					this.right.add(node);
+				}
+			} else {
+				if (this.left == null) {
+					this.left = node;
+					node.parent = this;
+				} else {
+					this.left.add(node);
+				}
+			}
+		}
+	}
+
+	public BTree tree = new BTree();
+
+	public void max1000(int no) {
+		boolean added = tree.add(no);
+		if (!added) {
+			System.out.println(
+					String.format("Min value in tree is: %s: | No: %s didn't added to max tree", tree.minValue.value, no));
+		}
+	}
+
 }
--- answers/src/test/java/com/my/codility/answers/SolutionTest.java
+++ answers/src/test/java/com/my/codility/answers/SolutionTest.java
@@ -1,5 +1,10 @@
 package com.my.codility.answers;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
 import org.junit.Test;
 
 /**
@@ -11,5 +16,42 @@ public class SolutionTest {
 	public void testSolution() {
 		Solution s = new Solution();
 
+		// first add 1000 random nos;
+
+		int min = Integer.MAX_VALUE;
+		int max = 0;
+		for(int i=0; i<Solution.BTree.MAX_COUNT; i++) {
+			int no = (int) Math.round(Math.random() * 10000);
+			if(no<min) {
+				min = no;
+			}
+			if(no > max) {
+				max = no;
+			}
+			s.max1000(no);
+		}
+
+		assertEquals(Solution.BTree.MAX_COUNT, s.tree.count);
+		assertEquals(min, s.tree.minValue.value);
+
+		// add another 1000 (or more or less)
+		for(int i=0; i<Solution.BTree.MAX_COUNT; i++) {
+			int no = (int) Math.round(Math.random() * 10000);
+			if(no>max) {
+				max = no;
+			}
+			s.max1000(no);
+		}
+
+		assertEquals(Solution.BTree.MAX_COUNT, s.tree.count);
+		List<Integer> list = s.tree.toList();
+
+		assertEquals(Solution.BTree.MAX_COUNT, list.size());
+		assertEquals(Integer.valueOf(max), list.get(0));
+
+		// list is sorted
+		for(int i=1; i<list.size(); i++) {
+			assertTrue(list.get(i-1) >= list.get(i));
+		}
 	}
 }
